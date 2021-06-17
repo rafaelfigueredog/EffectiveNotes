ccb8066c4b9550afe9eb738c9fa3e62569bb2e83
Rafael Guimarães <rafaelfigueredog@gmail.com>
2021-06-17 00:34:22 -0300

    Fix import method on NoteCard

--- src/components/NoteCard/index.js
+++ src/components/NoteCard/index.js
@@ -1,8 +1,14 @@
-import { Card, CardHeader, CardContent, IconButton, Typography, makeStyles, Box } from '@material-ui/core';
-import React,  {useState, useEffect} from 'react'
-import { AiFillDelete } from 'react-icons/ai'
+import React from 'react'
 import PaletteIcon from '@material-ui/icons/Palette';
 import PopoverColors from '../PopoverColors';
+import Card from '@material-ui/core/Card'
+import CardHeader from '@material-ui/core/CardHeader'
+import CardContent from '@material-ui/core/CardContent'
+import IconButton from '@material-ui/core/IconButton'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core';
+import { AiFillDelete } from 'react-icons/ai'
+
 
 const useStyles = makeStyles((theme) => {
     return {
@@ -21,11 +27,12 @@ const useStyles = makeStyles((theme) => {
 export default function NoteCard( {note, handleToDelete, palette, updateNotesColor} ) {
 
     const classes = useStyles(note)
+
     const handleChangeColor = (id) => {
         updateNotesColor(note.id, id)
     }
+
     return (
-    
        <Card elevation={3} className={classes.card} >
            <CardHeader className={classes.content} 
             title={ 
