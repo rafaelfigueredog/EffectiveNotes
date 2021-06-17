6e86e5f41a4c77d3bc3c766f0ea45e3f95196f52
Rafael Guimarães <rafaelfigueredog@gmail.com>
2021-06-17 00:38:46 -0300

    skeleton of productive view

--- src/components/ProductiveView/index.js
+++ src/components/ProductiveView/index.js
@@ -1,6 +1,50 @@
-import React from 'react' 
+import React, {useState} from 'react' 
+import {makeStyles} from '@material-ui/core/';
+import Typograpy from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
 
-export default function ProductiveView() {
+const useStyles = makeStyles((theme) => {
+    return {
+        page:{  
+            marginTop: theme.spacing(5), 
+        },
+    }
+  })
 
-    return <div></div>
+export default function ProductiveView({ notes, setNotes, palette }) {
+
+    const [todo, setTodo] = useState(notes)
+    const [inProgress, setInProgress] = useState([])
+    const [done, setDone] = useState([])
+
+    const classes = useStyles()
+
+    return (
+        <div className={classes.page} >
+            <Grid container direction="row" justify="space-around" alignItems="center" >
+
+                <Grid item  >
+                    { todo && <Typograpy variant='body1' color='textSecondary' > To-Do </Typograpy> }
+
+            
+                </Grid>
+
+                
+
+                <Grid item  >
+                    { inProgress && <Typograpy variant='body1' color='textSecondary' > Doing </Typograpy> }
+                    
+
+                </Grid>
+
+                
+
+                <Grid item  >
+                    { done && <Typograpy variant='body1' color='textSecondary' > Done </Typograpy> }
+
+                </Grid>
+
+            </Grid>
+        </div>
+    ); 
 }
